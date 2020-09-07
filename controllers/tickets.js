e7c16fa917a1a0e6f923e80f3bb5b7b076fc6ca3
Adam <adamjcoonen@gmail.com>
2020-09-07 11:27:44 -0400

    tickets checked and confirmed

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -18,18 +18,21 @@ function newTicket(req, res){
 
 function create(req, res) {
      //create a ticket object
-        let tickObj = {
+        const tickObj = {
+            flight: req.params.id,
             seat: req.body.seat,
             price: req.body.price,
-            flight: req.params.id
+            
         }
     const newTicket = new Ticket(tickObj);
-        newticket.save(function(err){
+        newTicket.save(function(err){
             if(err) {
                 res.render('ticket/new', {
                     flightID: req.params.id, title:'Add Ticket'
                 })
-                    res.redirect(`flights/${rea.params.id}`)
+            }else{
+                console.log(tickObj)
+                    res.redirect(`/flights/${req.params.id}`)
                 }
             })
 }
